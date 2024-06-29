7e73067a855ae5278fd3e3e917303e36128c6576
Andrea Jörgensen <contact@andreajorgensen.se>
2024-06-29 08:24:09 +0200

    Added remaining missing HUD info

--- src/actions/wildrp-player-stats.ts
+++ src/actions/wildrp-player-stats.ts
@@ -1,5 +1,5 @@
 import { action, KeyDownEvent, SingletonAction, WillAppearEvent, DidReceiveSettingsEvent, KeyUpEvent, Action, WillDisappearEvent } from "@elgato/streamdeck";
-import { IStreamDeckData, PlayerStatSvg, StreamDeckStats } from "./player-stat-enums";
+import { IStreamDeckData, PlayerStatSvg, StreamDeckStats, CombinedStatSvg } from "./player-stat-enums";
 import Mustache from "mustache";
 
 type PlayerStatsSettings = {
@@ -37,9 +37,11 @@ export class WildRPPlayerStats extends SingletonAction<PlayerStatsSettings> {
 			horseHealth: 0,
 			horseAlive: false,
 			showTelegramNotification: false,
-			playerIsInPvp: true,
+			playerIsInPvp: false,
 			isTalking: true,
 			voiceRangeIndex: 1,
+			showInjuredPersonNotification: false,
+			showLawNotification: false,
 		}
 		WildRPPlayerStats.instance = this;
 	}
@@ -65,6 +67,7 @@ export class WildRPPlayerStats extends SingletonAction<PlayerStatsSettings> {
 		let pct = 100;
 		let alive = this._stats.playerAlive && this._connected;
 		let horseAlive = this._stats.horseAlive && this._connected;
+		let combinedStats = settings.statType == StreamDeckStats.combinedStats;
 
 		switch (settings.statType) {
 			default:
@@ -105,9 +108,21 @@ export class WildRPPlayerStats extends SingletonAction<PlayerStatsSettings> {
 				if (pct < 20 && horseAlive) view.color = lowColor;
 				break;
 			case StreamDeckStats.showTelegramNotification:
-				view.telegram = true;
-				pct = this._stats.showTelegramNotification ? 100 : 0;
+				if (this._stats.showLawNotification)
+					view.lawIndicator = true;
+				else if (this._stats.showInjuredPersonNotification)
+					view.injuredIndicator = true;
+				else
+					view.telegram = true;
+
+				pct = this._stats.showTelegramNotification || this._stats.showInjuredPersonNotification || this._stats.showLawNotification ? 100 : 0;
 				view.color = this._stats.showTelegramNotification ? enabledColor : disabledColor;
+				view.color = this._stats.showInjuredPersonNotification || this._stats.showLawNotification ? "#F4CD69" : view.color;
+				break;
+			case StreamDeckStats.playerIsInPvp:
+				view.pvpIndicator = true;
+				pct = this._stats.playerIsInPvp ? 100 : 0;
+				view.color = this._stats.playerIsInPvp ? enabledColor : disabledColor;
 				break;
 			case StreamDeckStats.micRangeIndicator:
 				view.micRangeIndicator = true;
@@ -115,14 +130,49 @@ export class WildRPPlayerStats extends SingletonAction<PlayerStatsSettings> {
 				pct = this._connected ? 25 + this._stats.voiceRangeIndex * 25 : 0;
 				view.color = this._stats.isTalking ? lowColor : (this._connected ? enabledColor : disabledColor);
 				break;
+			case StreamDeckStats.playerTonicPercent:
+				view.tonic = true;
+				pct = this._stats.playerTonicPercent;
+				view.color = pct == 0 ? disabledColor : enabledColor;
+				break;
+			case StreamDeckStats.playerResSicknessPercent:
+				view.resSickness = true;
+				pct = this._stats.playerResSicknessPercent;
+				if (pct == 100) pct = 0;
+				view.color = pct == 0 ? disabledColor : enabledColor;
+				break;
+			case StreamDeckStats.combinedStats:
+				if (alive) {
+					view.color_hunger = this._stats.playerHunger < 20 ? lowColor : enabledColor;
+					view.color_thirst = this._stats.playerThirst < 20 ? lowColor : enabledColor;
+				}
+				else
+				{
+					view.color_hunger = disabledColor;
+					view.color_thirst = disabledColor;
+				}
+
+				view.percent_hunger = this.calculateSvgPercent(this._stats.playerHunger);
+				view.percent_thirst = this.calculateSvgPercent(this._stats.playerThirst);
+				view.percent_tonic = this.calculateSvgPercent(this._stats.playerTonicPercent);
+				view.percent_res = this.calculateSvgPercent(this._stats.playerResSicknessPercent);
+				if (this._stats.playerResSicknessPercent == 100) view.percent_res = this.calculateSvgPercent(0);
+
+				view.color_tonic = this._stats.playerTonicPercent == 0 ? disabledColor : enabledColor;
+				view.color_res = this._stats.playerResSicknessPercent == 0 || this._stats.playerResSicknessPercent == 100 ? disabledColor : enabledColor;
+				break;
 		}
 
-		view.percent =(1-(pct/100)) * 722.2;
+		view.percent = this.calculateSvgPercent(pct);
 
-		let image = Mustache.render(PlayerStatSvg, view);
+		let image = Mustache.render(combinedStats ? CombinedStatSvg : PlayerStatSvg, view);
 		let b = Buffer.from(image);
 
 		action.setImage("data:image/svg+xml;base64,"+b.toString("base64"));
+	}
+
+	calculateSvgPercent(value:number) {
+		return (1-(value/100)) * 722.2;
 	}
 
 	onWillAppear(ev: WillAppearEvent<PlayerStatsSettings>): void | Promise<void> {
